3c0ba4357b8d15a6fa3d5f4ed347aa06bc76c57d
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2025-02-03 18:46:36 +1100

    initial commit

---
+++ UserQueries.cpp
@@ -0,0 +1,51 @@
+﻿// UserQueries.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+#include <string>
+#include "UserQueries.h"
+#include <cpr/cpr.h>
+
+int main()
+{
+	std::string command;
+
+	
+
+	do {
+
+		std::cout << "Enter command: ";
+		std::cin >> command;
+
+
+
+
+		if (command == "get")
+		{
+
+		}
+		else if (command == "post")
+		{
+
+		}
+		else if (command == "put")
+		{
+
+		}
+		else if (command == "delete")
+		{
+
+		}
+		else if (command == "patch")
+		{
+
+		}
+		else if (command != "exit")
+		{
+			std::cout << "Incorrect input" << std::endl;
+		}
+
+	} while (command != "exit");
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
